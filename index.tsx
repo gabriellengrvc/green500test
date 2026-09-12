66a0e8b9ed91d30997a2b298fe8d2cf9e14a3e21
Gabrielle Gervacio <140438379+gabriellengrvc@users.noreply.github.com>
2026-09-12 14:32:44 +0200

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,274 @@
+import { imgButtons } from "./svg-jq3xm";
+
+function FillShadow() {
+  return (
+    <div className="absolute inset-0 rounded-[1000px] shadow-[1.25px_0px_0px_-0.75px_#d0d0d0,-1.25px_0px_0px_-0.75px_#d0d0d0,0px_0px_0px_0.5px_#e8e8e8,0px_8px_15px_0px_rgba(0,0,0,0.02)]" data-name="Fill + Shadow">
+      <div aria-hidden className="absolute inset-0 pointer-events-none rounded-[1000px]">
+        <div className="absolute inset-0 rounded-[1000px]" style={{ backgroundImage: "linear-gradient(90deg, rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0.25) 100%), linear-gradient(90deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.25) 100%)" }} />
+        <div className="absolute bg-[rgba(68,68,68,0.6)] inset-0 mix-blend-plus-lighter rounded-[1000px]" />
+        <div className="absolute bg-[rgba(248,248,248,0.2)] inset-0 mix-blend-luminosity rounded-[1000px]" />
+      </div>
+    </div>
+  );
+}
+
+function GlassEffect() {
+  return (
+    <div className="absolute inset-0 pointer-events-none rounded-[1000px]" data-name="Glass Effect">
+      <div aria-hidden className="absolute bg-[rgba(0,0,0,0)] inset-0 rounded-[1000px]" />
+      <div className="absolute inset-0 rounded-[inherit] shadow-[inset_0px_40px_10px_-40px_#282828,inset_0px_-40px_10px_-40px_#282828,inset_0px_40px_30px_-40px_#e6e6e6]" />
+    </div>
+  );
+}
+
+function Leading() {
+  return (
+    <div className="content-stretch flex flex-[1_0_0] gap-[12px] items-center min-w-px relative" data-name="Leading">
+      <div className="h-[44px] min-w-[44px] relative rounded-[296px] shrink-0" data-name="Button Group 1">
+        <div className="flex flex-row items-center justify-center min-w-[inherit] size-full">
+          <div className="content-stretch flex gap-[12px] items-center justify-center min-w-[inherit] px-[2px] relative size-full">
+            <div className="-translate-y-1/2 absolute h-[44px] left-0 right-0 top-1/2" data-name="BG">
+              <FillShadow />
+              <GlassEffect />
+            </div>
+            <div className="h-[36px] min-w-[36px] relative rounded-[100px] shrink-0" data-name="Text">
+              <div className="flex flex-row items-center justify-center min-w-[inherit] size-full">
+                <div className="content-stretch flex gap-[2px] items-center justify-center min-w-[inherit] px-[8px] relative size-full">
+                  <div className="[word-break:break-word] flex flex-col font-['SF_Pro:Semibold',sans-serif] font-[590] justify-center leading-[0] relative shrink-0 text-[#1a1a1a] text-[17px] text-center whitespace-nowrap" style={{ fontVariationSettings: '"wdth" 100', fontFeatureSettings: '"ss16" 1' }}>
+                    <p className="leading-[normal]">{`\u{100BF6}`}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function FillShadow1() {
+  return (
+    <div className="absolute inset-0 rounded-[1000px] shadow-[1.25px_0px_0px_-0.75px_#d0d0d0,-1.25px_0px_0px_-0.75px_#d0d0d0,0px_0px_0px_0.5px_#e8e8e8,0px_8px_15px_0px_rgba(0,0,0,0.02)]" data-name="Fill + Shadow">
+      <div aria-hidden className="absolute inset-0 pointer-events-none rounded-[1000px]">
+        <div className="absolute inset-0 rounded-[1000px]" style={{ backgroundImage: "linear-gradient(90deg, rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0.25) 100%), linear-gradient(90deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.25) 100%)" }} />
+        <div className="absolute bg-[rgba(68,68,68,0.6)] inset-0 mix-blend-plus-lighter rounded-[1000px]" />
+        <div className="absolute bg-[rgba(248,248,248,0.2)] inset-0 mix-blend-luminosity rounded-[1000px]" />
+      </div>
+    </div>
+  );
+}
+
+function GlassEffect1() {
+  return (
+    <div className="absolute inset-0 pointer-events-none rounded-[1000px]" data-name="Glass Effect">
+      <div aria-hidden className="absolute bg-[rgba(0,0,0,0)] inset-0 rounded-[1000px]" />
+      <div className="absolute inset-0 rounded-[inherit] shadow-[inset_0px_40px_10px_-40px_#282828,inset_0px_-40px_10px_-40px_#282828,inset_0px_40px_30px_-40px_#e6e6e6]" />
+    </div>
+  );
+}
+
+function FillShadow2() {
+  return (
+    <div className="absolute inset-0 rounded-[1000px] shadow-[1.25px_0px_0px_-0.75px_#d0d0d0,-1.25px_0px_0px_-0.75px_#d0d0d0,0px_0px_0px_0.5px_#e8e8e8,0px_8px_15px_0px_rgba(0,0,0,0.02)]" data-name="Fill + Shadow">
+      <div aria-hidden className="absolute inset-0 pointer-events-none rounded-[1000px]">
+        <div className="absolute inset-0 rounded-[1000px]" style={{ backgroundImage: "linear-gradient(90deg, rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0.25) 100%), linear-gradient(90deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.25) 100%)" }} />
+        <div className="absolute bg-[rgba(68,68,68,0.6)] inset-0 mix-blend-plus-lighter rounded-[1000px]" />
+        <div className="absolute bg-[rgba(248,248,248,0.2)] inset-0 mix-blend-luminosity rounded-[1000px]" />
+      </div>
+    </div>
+  );
+}
+
+function GlassEffect2() {
+  return (
+    <div className="absolute inset-0 pointer-events-none rounded-[1000px]" data-name="Glass Effect">
+      <div aria-hidden className="absolute bg-[rgba(0,0,0,0)] inset-0 rounded-[1000px]" />
+      <div className="absolute inset-0 rounded-[inherit] shadow-[inset_0px_40px_10px_-40px_#282828,inset_0px_-40px_10px_-40px_#282828,inset_0px_40px_30px_-40px_#e6e6e6]" />
+    </div>
+  );
+}
+
+function Trailing() {
+  return (
+    <div className="content-stretch flex flex-[1_0_0] gap-[12px] items-center justify-end min-w-px relative" data-name="Trailing">
+      <div className="h-[44px] relative rounded-[296px] shrink-0" data-name="Button Group">
+        <div className="flex flex-row items-center size-full">
+          <div className="content-stretch flex gap-[20px] items-center px-[4px] relative size-full">
+            <div className="-translate-y-1/2 absolute h-[44px] left-0 right-0 top-1/2" data-name="BG">
+              <FillShadow1 />
+              <GlassEffect1 />
+            </div>
+            <div className="relative rounded-[100px] shrink-0 size-[36px]" data-name="Symbol 1">
+              <div className="content-stretch flex flex-col items-start relative size-full">
+                <div className="[word-break:break-word] flex flex-[1_0_0] flex-col font-['SF_Pro:Medium',sans-serif] font-[510] justify-center leading-[0] min-h-px relative text-[#1a1a1a] text-[17px] text-center w-full" style={{ fontVariationSettings: '"wdth" 100', fontFeatureSettings: '"ss16" 1' }}>
+                  <p className="leading-[normal]">{`\u{1004D4}`}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="h-[44px] relative rounded-[296px] shrink-0" data-name="Button Group">
+        <div className="flex flex-row items-center size-full">
+          <div className="content-stretch flex gap-[20px] items-center px-[4px] relative size-full">
+            <div className="-translate-y-1/2 absolute h-[44px] left-0 right-0 top-1/2" data-name="BG">
+              <FillShadow2 />
+              <GlassEffect2 />
+            </div>
+            <div className="relative rounded-[100px] shrink-0 size-[36px]" data-name="Symbol 1">
+              <div className="content-stretch flex flex-col items-start relative size-full">
+                <div className="[word-break:break-word] flex flex-[1_0_0] flex-col font-['SF_Pro:Medium',sans-serif] font-[510] justify-center leading-[0] min-h-px relative text-[#1a1a1a] text-[17px] text-center w-full" style={{ fontVariationSettings: '"wdth" 100', fontFeatureSettings: '"ss16" 1' }}>
+                  <p className="leading-[normal]">{`\u{1004D4}`}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Controls() {
+  return (
+    <div className="content-stretch flex items-start relative shrink-0 w-full" data-name="Controls">
+      <Leading />
+      <Trailing />
+    </div>
+  );
+}
+
+function TitleAndSubtitle() {
+  return (
+    <div className="min-h-[48px] relative shrink-0 w-full" data-name="Title and Subtitle">
+      <div className="content-stretch flex flex-col gap-[2px] items-start min-h-[inherit] pt-[4px] px-[10px] relative size-full">
+        <div className="h-[41px] relative shrink-0 w-full" data-name="Title">
+          <p className="[word-break:break-word] absolute font-['SF_Pro:Bold',sans-serif] font-bold leading-[41px] left-0 right-0 text-[#1a1a1a] text-[34px] top-0 tracking-[0.4px] whitespace-nowrap" style={{ fontVariationSettings: '"wdth" 100' }}>
+            Title
+          </p>
+        </div>
+        <div className="relative shrink-0" data-name="Subtitle">
+          <div className="flex flex-row items-center justify-center size-full">
+            <div className="content-stretch flex items-center justify-center relative size-full">
+              <p className="[word-break:break-word] font-['SF_Pro:Medium',sans-serif] font-[510] leading-[20px] relative shrink-0 text-[#727272] text-[15px] tracking-[-0.23px] whitespace-nowrap" style={{ fontVariationSettings: '"wdth" 100' }}>
+                Subtitle
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function FillShadow3() {
+  return (
+    <div className="absolute inset-0 rounded-[1000px] shadow-[1.25px_0px_0px_-0.75px_#d0d0d0,-1.25px_0px_0px_-0.75px_#d0d0d0,0px_0px_0px_0.5px_#e8e8e8,0px_8px_15px_0px_rgba(0,0,0,0.02)]" data-name="Fill + Shadow">
+      <div aria-hidden className="absolute inset-0 pointer-events-none rounded-[1000px]">
+        <div className="absolute inset-0 rounded-[1000px]" style={{ backgroundImage: "linear-gradient(90deg, rgba(255, 255, 255, 0.25) 0%, rgba(255, 255, 255, 0.25) 100%), linear-gradient(90deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, 0.25) 100%)" }} />
+        <div className="absolute bg-[rgba(68,68,68,0.6)] inset-0 mix-blend-plus-lighter rounded-[1000px]" />
+        <div className="absolute bg-[rgba(248,248,248,0.2)] inset-0 mix-blend-luminosity rounded-[1000px]" />
+      </div>
+    </div>
+  );
+}
+
+function GlassEffect3() {
+  return (
+    <div className="absolute inset-0 pointer-events-none rounded-[1000px]" data-name="Glass Effect">
+      <div aria-hidden className="absolute bg-[rgba(0,0,0,0)] inset-0 rounded-[1000px]" />
+      <div className="absolute inset-0 rounded-[inherit] shadow-[inset_0px_40px_10px_-40px_#282828,inset_0px_-40px_10px_-40px_#282828,inset_0px_40px_30px_-40px_#e6e6e6]" />
+    </div>
+  );
+}
+
+function Buttons() {
+  return (
+    <div className="content-stretch flex h-[36px] items-center justify-center relative shrink-0" data-name="Buttons">
+      <div className="relative rounded-[100px] shrink-0" data-name="Tab 1">
+        <div className="flex flex-row items-center justify-center size-full">
+          <div className="content-stretch flex gap-[10px] items-center justify-center px-[18px] py-[8px] relative size-full">
+            <div className="absolute bg-[#ededed] inset-0 rounded-[100px]" data-name="Selection" />
+            <div className="content-stretch flex gap-[10px] items-center justify-center relative shrink-0" data-name="Frame">
+              <div className="absolute bg-[rgba(255,255,255,0.5)] inset-[-8px_-18px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[19.749px_11.853px] mask-size-[37.642px_11.316px]" style={{ maskImage: `url("${imgButtons}")` }} data-name="FFFFFF @50%" />
+              <div className="absolute bg-[#08f] inset-[-8px_-18px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[19.749px_11.853px] mask-size-[37.642px_11.316px]" style={{ maskImage: `url("${imgButtons}")` }} data-name="Tint (Plus D)" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative shrink-0" data-name="Tab 2">
+        <div className="flex flex-row items-center justify-center size-full">
+          <div className="content-stretch flex items-center justify-center px-[18px] py-[8px] relative size-full">
+            <div className="[word-break:break-word] flex flex-col font-['SF_Pro:Medium',sans-serif] font-[510] justify-center leading-[0] overflow-hidden relative shrink-0 text-[#1a1a1a] text-[15px] text-center text-ellipsis tracking-[-0.23px] whitespace-nowrap" style={{ fontVariationSettings: '"wdth" 100' }}>
+              <p className="leading-[20px] overflow-hidden text-ellipsis">Tab 2</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative shrink-0" data-name="Tab 3">
+        <div className="flex flex-row items-center justify-center size-full">
+          <div className="content-stretch flex items-center justify-center px-[18px] py-[8px] relative size-full">
+            <div className="[word-break:break-word] flex flex-col font-['SF_Pro:Medium',sans-serif] font-[510] justify-center leading-[0] overflow-hidden relative shrink-0 text-[#1a1a1a] text-[15px] text-center text-ellipsis tracking-[-0.23px] whitespace-nowrap" style={{ fontVariationSettings: '"wdth" 100' }}>
+              <p className="leading-[20px] overflow-hidden text-ellipsis">Tab 3</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative shrink-0" data-name="Tab 4">
+        <div className="flex flex-row items-center justify-center size-full">
+          <div className="content-stretch flex items-center justify-center px-[18px] py-[8px] relative size-full">
+            <div className="[word-break:break-word] flex flex-col font-['SF_Pro:Medium',sans-serif] font-[510] justify-center leading-[0] overflow-hidden relative shrink-0 text-[#1a1a1a] text-[15px] text-center text-ellipsis tracking-[-0.23px] whitespace-nowrap" style={{ fontVariationSettings: '"wdth" 100' }}>
+              <p className="leading-[20px] overflow-hidden text-ellipsis">Tab 4</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative shrink-0" data-name="Tab 5">
+        <div className="flex flex-row items-center justify-center size-full">
+          <div className="content-stretch flex items-center justify-center px-[18px] py-[8px] relative size-full">
+            <div className="[word-break:break-word] flex flex-col font-['SF_Pro:Medium',sans-serif] font-[510] justify-center leading-[0] overflow-hidden relative shrink-0 text-[#1a1a1a] text-[15px] text-center text-ellipsis tracking-[-0.23px] whitespace-nowrap" style={{ fontVariationSettings: '"wdth" 100' }}>
+              <p className="leading-[20px] overflow-hidden text-ellipsis">Tab 5</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default function ToolbarTopIPad() {
+  return (
+    <div className="relative size-full" data-name="Toolbar - Top - iPad">
+      <div className="content-stretch flex flex-col gap-[10px] items-start pb-[10px] px-[10px] relative size-full">
+        <Controls />
+        <TitleAndSubtitle />
+        <div className="-translate-x-1/2 absolute left-[calc(50%+5.5px)] top-0" data-name="Tab Bar">
+          <div className="content-stretch flex items-start p-[4px] relative size-full">
+            <FillShadow3 />
+            <GlassEffect3 />
+            <div className="relative shrink-0 w-[54px]" data-name="Sidebar">
+              <div className="flex flex-row items-center justify-center size-full">
+                <div className="content-stretch flex items-center justify-center px-[16px] py-[8px] relative size-full">
+                  <div className="[word-break:break-word] flex flex-col font-['SF_Pro:Medium',sans-serif] font-[510] justify-center leading-[0] relative shrink-0 text-[#1a1a1a] text-[15px] text-center tracking-[-0.23px] whitespace-nowrap" style={{ fontVariationSettings: '"wdth" 100' }}>
+                    <p className="leading-[20px]">{`\u{100C31}`}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <Buttons />
+            <div className="relative shrink-0 w-[54px]" data-name="Search">
+              <div className="flex flex-row items-center justify-center size-full">
+                <div className="content-stretch flex items-center justify-center px-[16px] py-[8px] relative size-full">
+                  <div className="[word-break:break-word] flex flex-col font-['SF_Pro:Medium',sans-serif] font-[510] justify-center leading-[0] relative shrink-0 text-[#1a1a1a] text-[15px] text-center tracking-[-0.23px] whitespace-nowrap" style={{ fontVariationSettings: '"wdth" 100' }}>
+                    <p className="leading-[20px]">{`\u{100C31}`}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
